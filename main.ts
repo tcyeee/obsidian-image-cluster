e1fe59a73f9436413702bc29d2d9550db3b4dd25
tcyeee <tcyeee@outlook.com>
2026-07-03 16:31:32 +0800

    feat: wire up editor drop target in plugin entry point

--- main.ts
+++ main.ts
@@ -3,6 +3,7 @@ import { addImageLayoutMarkdownProcessor } from "./src/render/processor";
 import { registerEditorMenu } from "./src/editor-menu";
 import { registerHoverGroupTrigger } from "./src/hover-group-trigger";
 import { registerImageDragSource } from "./src/image-drag-source";
+import { registerEditorDropTarget } from "./src/editor-drop-target";
 import { ImgRowPluginSettings, DEFAULT_SETTINGS, ImgRowSettingTab, applySettingsToConfig } from "./src/settings";
 
 export default class ImgRowPlugin extends Plugin {
@@ -19,6 +20,8 @@ export default class ImgRowPlugin extends Plugin {
 		registerHoverGroupTrigger(this);
 		// 注册独立图片拖入已有图片组的功能
 		registerImageDragSource(this);
+		// 注册"拖出图片组"落到编辑器空白处的功能
+		registerEditorDropTarget(this);
 		// 注册设置页
 		this.addSettingTab(new ImgRowSettingTab(this.app, this));
 	}
